e5516df6adbb6f9c118aa4590aa52a6fa0430aaf
Aradhya Yadav <aradhyay868@gmail.com>
2024-06-22 15:47:48 +0530

    Add .gitattributes file and normalize line endings

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+"use client" ;
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
 
-function Navbar() {
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
   return (
-    <div>Navbar</div>
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)} >
+      <Menu setActive={setActive}>
+       <Link href={"/"}>
+       <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+       </Link>
+       <Link href={"/about"}>
+       <MenuItem setActive={setActive} active={active} item="about"></MenuItem>
+       </Link>
+       <Link href={"/contact"}>
+       <MenuItem setActive={setActive} active={active} item="contact"></MenuItem>
+       </Link>
+      </Menu>
+        
+        </div>
   )
 }
 
